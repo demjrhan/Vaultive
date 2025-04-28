272a6951979d49fbb8da862c2a30ae92db1495cd
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-04-28 13:10:59 +0200

    made the data into seperate file

---
+++ src/movies.js
@@ -0,0 +1,92 @@
+// data.js
+
+export const featuredMovie = {
+  publisher: 'A Disney Original Film',
+  title: 'RAYA AND THE LAST DRAGON',
+  imageSrc: 'public/img/star/raya-the-last-dragon-star-container.png',
+  backgroundGif: '../public/gif/raya.gif',
+  backgroundHolder: '../public/img/star/raya-the-last-dragon-background.png',
+};
+
+export const recommendations = [
+  { id: 'pulpfiction', src: './public/img/pulpfiction-poster.png', alt: 'Pulp Fiction' },
+  { id: 'avengers', src: './public/img/avengers-poster.png', alt: 'Avengers' },
+  { id: 'scarface', src: 'public/img/scarface-poster.png', alt: 'Scarface' },
+  { id: 'deadpool', src: './public/img/deadpool-poster.png', alt: 'Deadpool' },
+  { id: 'johnwick', src: './public/img/john-wick-poster.png', alt: 'John Wick' },
+  { id: 'raya', src: './public/img/raya-the-last-dragon-poster.png', alt: 'Raya' },
+  { id: 'godfather', src: './public/img/godfather-poster.png', alt: 'God Father' },
+];
+
+export const details = [
+  {
+    id: 'raya',
+    src: 'public/img/raya-the-last-dragon-poster.png',
+    alt: 'Raya',
+    title: 'Raya',
+    description:
+      'Raya is a skilled warrior on a quest to find the last dragon...',
+    background: "url('../public/img/star/raya-the-last-dragon-background.png')",
+    backgroundGif: "url('../public/gif/raya.gif')",
+  },
+  {
+    id: 'johnwick',
+    src: 'public/img/john-wick-poster.png',
+    alt: 'John Wick',
+    title: 'John Wick',
+    description:
+      'John Wick is a former assassin drawn back into the criminal underworld...',
+    background: "url('../public/img/john-wick-background.png')",
+    backgroundGif: "url('../public/gif/john-wick2.gif')",
+  },
+  {
+    id: 'deadpool',
+    src: 'public/img/deadpool-poster.png',
+    alt: 'Deadpool',
+    title: 'Deadpool',
+    description:
+      'Deadpool tells the story of Wade Wilson, a former special forces operative...',
+    background: "url('../public/img/deadpool-background.png')",
+    backgroundGif: "url('../public/gif/deadpool.gif')",
+  },
+  {
+    id: 'pulpfiction',
+    src: 'public/img/pulpfiction-poster.png',
+    alt: 'Pulp Fiction',
+    title: 'Pulp Fiction',
+    description:
+      'Pulp Fiction is a cult classic directed by Quentin Tarantino...',
+    background: "url('../public/img/pulpfiction-background.png')",
+    backgroundGif: "url('../public/gif/pulpfiction.gif')",
+  },
+  {
+    id: 'avengers',
+    src: 'public/img/avengers-poster.png',
+    alt: 'Avengers',
+    title: 'Avengers',
+    description:
+      "Avengers brings together Marvel's greatest superheroes in an epic battle...",
+    background: "url('../public/img/avengers-background.png')",
+    backgroundGif: "url('../public/gif/avengers.gif')",
+  },
+  {
+    id: 'godfather',
+    src: 'public/img/godfather-poster.png',
+    alt: 'God Father',
+    title: 'God Father',
+    description:
+      'The Godfather is a cinematic masterpiece that chronicles the rise of Michael Corleone...',
+    background: "url('../public/img/godfather-background.png')",
+    backgroundGif: "url('../public/gif/godfather.gif')",
+  },
+  {
+    id: 'scarface',
+    src: 'public/img/scarface-poster.png',
+    alt: 'Scarface',
+    title: 'Scarface',
+    description:
+      "Scarface follows the rise and fall of Tony Montana, a Cuban immigrant...",
+    background: "url('../public/img/scarface-background.png')",
+    backgroundGif: "url('../public/gif/scarface.gif')",
+  },
+];
