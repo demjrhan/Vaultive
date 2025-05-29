001d8687b4571830a796ed5a7ca90e54472c85db
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-05-30 00:55:43 +0200

    updated background video size and lilo stitch poster

--- Backend/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ Backend/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac2b4033435f563f13c2085bab0e63c3634f988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43392b492642e17adb7cb7f993957b22c81368e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
