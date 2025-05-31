c04107fe33df73ee05fcee55e023b42b753bc4b4
demirhan <120950077+demirhanylcn@users.noreply.github.com>
2025-05-31 20:23:20 +0200

    updated documentation

--- Backend/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
+++ Backend/Project/obj/Debug/net9.0/Project.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b41cd65563fa6557c04ebddbdee448392a6c5264")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0f25ee4042d29064819b31873395517624e0af")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
